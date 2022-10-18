7d8226e77979dc4c16bd560a4667bed7c1968035
Urvino <castelanu91@gmail.com>
2022-10-17 18:01:30 -0700

    Starting next function

--- game.js
+++ game.js
@@ -2,7 +2,6 @@
 //Rock,paper or scissors
 //remember to use console.log; to output results.
 
-
 function getComputerChoice() {
     let rock = "Rock";
     let paper = "Paper";
@@ -20,3 +19,27 @@ function getComputerChoice() {
         return scissors;
     }
 }
+
+//Write a function that plays a single round of Rock Paper Scissors.
+//The function should take two parameters - the playerSelection and computerSelection
+//return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+//make sure function is case-insentive, user can input rock, RocK, ROCK
+//you want to return the results of this function call, not console.log() them
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
+
+//Write a NEW function called game(). Call the playRound function inside of this one 
+//to play a 5 round game that keeps score and reports a winner or loser at the end
+
+for (let i = 0; i < 5; i++) {
+    // your code here!
+ }
+
+ //you should be using console.log() to display the results of each round and the winner at the end.
